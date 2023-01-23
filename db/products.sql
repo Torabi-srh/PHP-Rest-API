cd9d335f7be45d134b65c22ca875b70497abacd4
Soroush Trb <tspersian@gmail.com>
2023-01-23 18:58:13 +0330

    .

--- db/products.sql
+++ db/products.sql
@@ -16,7 +16,6 @@
 
 SET NAMES utf8mb4;
 SET FOREIGN_KEY_CHECKS = 0;
-CREATE DATABASE IF NOT EXISTS local;
 -- ----------------------------
 -- Table structure for products
 -- ----------------------------
